9d10664d4e4d1c27832ab9171750810f6b7fe054
mickeykim70 <mickeykim70@gmail.com>
2024-04-27 22:05:36 +0900

    This is my work also

--- snake/scoreboard.py
+++ snake/scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "bold")
 
 
 class Scoreboard(Turtle):
@@ -9,4 +11,16 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 250)
         self.color("white")
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
--- snake/snake.py
+++ snake/snake.py
@@ -15,11 +15,18 @@ class Snake():
 
     def create_snake(self):
         for pos in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(pos)
-            self.segments.append(new_segment)
+            self.add_segment(pos)
+
+    def add_segment(self, pos):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(pos)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        # add a new segement
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
