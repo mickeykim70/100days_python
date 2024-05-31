f7e376116ebc21a88aa234f01e9e223bc21172b5
mickeykim70 <mickeykim70@gmail.com>
2024-05-31 13:39:49 +0900

    defing class

---
+++ quiz_game_2/question_model.py
@@ -0,0 +1,13 @@
+class Question():
+    
+    def __init__(self, q_text, q_answer) -> None:
+        self.text = q_text
+        self.answer = q_answer
+        
+        
+new_q = Question("df","false")
+    
+
+        
+print(new_q.text)
+
