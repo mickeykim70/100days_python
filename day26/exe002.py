b1799cd929786ec948b6cae98a1bcbc03c424c68
mickeykim70 <mickeykim70@gmail.com>
2024-06-10 17:52:51 +0900

    state guess

--- day26/exe002.py
+++
@@ -1,8 +0,0 @@
-sentence = input("Enter a sentence: ")
-
-result = sentence.split()
-print(result)
-
-char_count = {word: len(word) for word in result}
-
-print(char_count)
--- us_state_game/main_old.py
+++ us_state_game/main_old.py
@@ -1,5 +1,3 @@
-
-
 import turtle
 import pandas as pd
 
