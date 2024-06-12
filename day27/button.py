2a8ee18f5f66a5f4cfbc06d1d265d02c9ef70e74
MickeyKim70 <mickeykim70@gmail.com>
2024-06-12 23:46:01 +0900

    miles to km

--- day27/button.py
+++ day27/button.py
@@ -5,27 +5,26 @@ window = tk.Tk()
 window.title("Widget Examples")
 window.minsize(500, 600)
 
-#Label
-
-my_label = tk.Label(text='Hello World', font=('Arial', 15))
-my_label.pack()
-
 
 def click_me():
     i = input_text.get()
     my_label.config(text=i)
 
-
+#Label
+my_label = tk.Label(text='Hello World', font=('Arial', 15))
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
 #Button
-my_button = tk.Button(text="Click Me", command=click_me)
-my_button.pack()
+my_button1 = tk.Button(text="Click Me", command=click_me)
+my_button1.grid(column=2, row=0)
+
+my_button2 = tk.Button(text="2nd button", command=click_me)
+my_button2.grid(column=1, row=1)
 
 #Entry
 input_text = tk.Entry(width=10)
-
-input_text.pack()
-
+input_text.grid(column=3, row=2)
 
 
 window.mainloop()
